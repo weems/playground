445c9f960eaab18bc51a80cae212fa2ee2101cc1
Michael Weems <weems@weems.ca>
2014-04-23 23:11:37 -0500

    Create outputs.py

---
+++ outputs.py
@@ -0,0 +1,11 @@
+#! /bin/env python3
+import os
+name = input("Enter your Name: ")
+print("Nice to meet you", name)
+#create a directory "test"
+os.mkdir(test)
+def foo():
+  count = 1
+  msg = a string
+  print("", msg)
+  print("",count)
